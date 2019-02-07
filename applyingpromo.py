b9083469c558866fda6e46322ec3b0bce06f060e
John Pham <phammyjohn@hotmail.com>
2019-02-07 12:52:26 +1100

    Update applyingpromo.py

--- applyingpromo.py
+++ applyingpromo.py
@@ -19,22 +19,26 @@ class shopping():
             print("You are applying with no family members, you will not recieve a discount")
 
     def products(self,items=None):
-        self.items = items 
-        self.cost = self.items * 20 ## assuming each item costs 20 dollars
+        self.items = items
+        self.cost = self.items * 20 ## assuming each item is 20 dollars each
         try:
             print("Checking your products now")
             if self.items is None:
                 print("You have not checked out any items")
                 return False
-            elif self.age <= 13 and self.family <= 3:
+            elif self.age >= 25 and self.family <= 2:
                 print("You are eligible for a promo of 20% woo hoo")
-                self.cost *= 0.8
-                print("Now your cost is ${}".format(str(self.cost)))
+                self.discount = self.cost * 0.8
+                print("Your original cost was ${}".format(str(self.cost)))
+                print("Your cost is now ${}".format(str(self.discount)))
+                print("You have saved: ${}".format(self.cost - self.discount))
                 return(self.items)
             elif self.age <= 15 and self.family <= 4:
                 print("You have a eligible discount of 50% :)")
-                self.cost *= 0.5
-                print("Your cost is now ${}".format(str(self.cost)))
+                self.discount = self.cost * 0.5
+                print("Your original cost was ${}".format(str(self.cost)))
+                print("Your cost is now ${}".format(str(self.discount)))
+                print("You have saved: ${}".format(self.cost - self.discount))
                 return(self.items)
             else:
                 print("You are not allowed a promotion :(, Promotions are only avaliable for families with kids and of a certain age")
@@ -52,7 +56,7 @@ class shopping():
 
 
 def main():
-    JohnPham = shopping('John','Pham', 15, 3)
+    JohnPham = shopping('John','Pham', 15, 4)
     jphamproducts = JohnPham.products(6)
 
 
