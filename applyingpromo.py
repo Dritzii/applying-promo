c69d2229d3d15d7a9697185b2c43e573338b56e7
John Pham <phammyjohn@hotmail.com>
2019-02-07 11:34:38 +1100

    Update applyingpromo.py

--- applyingpromo.py
+++ applyingpromo.py
@@ -4,31 +4,43 @@ Author: John Pham
 
 """
 
-class person(object):
-    def __init__(self,Fname,Lname,Age,Cost,Family=None):
+class shopping():
+    def __init__(self,Fname,Lname,Age,Family=None):
         self.firstname = Fname
         self.lastname = Lname
         self.age = Age
-        self.cost = Cost
         print("Hello {} it is time to add you onto our system :)".format(str(Fname)))
         if Family is None:
             self.family = ''
-        else:
+        elif Family is not None:
             self.family = Family
-            print("You have {} people in your family :)".format(str(self.family)))
-        if Age <= 13 and Family is not None:
-            print("You are eligible for a promo of 50% woo hoo because you are under 13 :)")
-            self.cost *= 0.5
-            print("Now your cost is ${} yippee".format(str(self.cost)))
-        elif Age <= 15 and Family <= 4:
-            print("You have a eligible discount of 20% :)")
-            self.cost *= 0.8
-            print("Your cost is now ${}".format(str(self.cost)))
+            print("You have {} people in your family, You may be eligible for a discount :)".format(str(self.family)))
         else:
-            print("You are not allowed a promotion :(, Promotions are only avaliable for families with kids")
+            print("You are applying with no family members")
 
-
-    
+    def products(self,items=None):
+        self.items = items
+        self.cost = self.items * 20
+        try:
+            print("Checking your products now")
+            if items is None:
+                print("You have not checked out any items")
+                return False
+            elif self.age <= 13 and self.family <= 3:
+                print("You are eligible for a promo of 20% woo hoo")
+                self.cost *= 0.8
+                print("Now your cost is ${} yippee".format(str(self.cost)))
+                return(self.items)
+            elif self.age <= 15 and self.family <= 4:
+                print("You have a eligible discount of 50% :)")
+                self.cost *= 0.5
+                print("Your cost is now ${}".format(str(self.cost)))
+                return(self.items)
+            else:
+                print("You are not allowed a promotion :(, Promotions are only avaliable for families with kids and of a certain age")
+                return False
+        except:
+            print("Error")
 
         
 
@@ -39,10 +51,19 @@ class person(object):
 
 
 
-
-
 def main():
-    JohnPham = person('John','Pham', 15, 50, 3)
+    JohnPham = shopping('John','Pham', 15, 3)
+    jphamproducts = JohnPham.products(6)
+
+
+
+
+
+
+
+
+
+
 
 
 
